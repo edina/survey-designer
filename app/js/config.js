7723955491eb987694c719cc5ca5821d84ecd634
Panos Terzis <panagiwtis.terzis@gmail.com>
2015-05-20 10:03:23 +0100

    start with the design

---
+++ app/js/config.js
@@ -0,0 +1,16 @@
+var config = {
+    "options": [
+        "text",
+        "range",
+        "textarea",
+        "checkbox",
+        "radio",
+        "select",
+        "photo",
+        "audio",
+        "warning",
+        "los",
+        "gps",
+        "dtree"
+    ]
+}
