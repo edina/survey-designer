a55c4d77ee4dd0e538bfc1468218d0f7d553003f
Panos Terzis <P.Terzis@ed.ac.uk>
2015-06-05 10:48:53 +0100

    make app.css relative path

--- app/router.js
+++ app/router.js
@@ -1,7 +1,7 @@
 import Backbone from 'backbone';
 import { SurveyView } from './views/Survey';
 import { MenuView } from './views/Menu';
-import 'styles/app.css!';
+import './styles/app.css!';
 
 
 export class SurveyDesigner extends Backbone.Router {
