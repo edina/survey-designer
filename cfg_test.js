f0d7c00fc67721b39695284eab1778ae56701f50
panterz <panagiwtis.terzis@gmail.com>
2016-01-13 16:45:39 +0000

    add cfg test for travis

---
+++ cfg_test.js
@@ -0,0 +1,21 @@
+var cfg = {
+        "baseurl": "",
+        "pcapianonymous": "00000000-0000-0000-0000-000000000000",
+        "userid": "00000000-0000-0000-0000-000000000000",
+        "version": "1.3",
+        "forms-list": true,
+        "options": [
+            "text",
+            "range",
+            "textarea",
+            "checkbox",
+            "radio",
+            "select",
+            "image",
+            "audio",
+            "warning",
+            "gps",
+            "help",
+            "dtree"
+        ]
+};
