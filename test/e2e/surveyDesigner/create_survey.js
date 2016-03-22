fb352c3eed83470f9711eb8182ccc6da02752e42
panterz <panagiwtis.terzis@gmail.com>
2016-03-22 16:53:29 +0000

    #12: added tests for text, range, radio

--- test/e2e/surveyDesigner/create_survey.js
+++ test/e2e/surveyDesigner/create_survey.js
@@ -1,3 +1,6 @@
+var fs = require('fs');
+var editor = JSON.parse(fs.readFileSync('test/test.json').toString('utf8'));
+
 module.exports = {
     'Demo test survey designer' : function (browser) {
         browser
@@ -8,20 +11,121 @@ module.exports = {
             .isVisible('.dropdown-menu')
             .isVisible('button[id=add-attribute]')
             .isVisible('button[id=define-bbox]')
-            .pause(1000)
+            .pause(1000);
     },
 
     'Add an attribute': function (browser) {
         browser
             .click('button[id=add-attribute]')
-            .waitForElementVisible('input[name=attribute-key]', 1000)
+            .waitForElementVisible('input[name=attribute-key]', 1000);
+        for (var key in editor.extra) {
+            browser
+                .setValue('input[name=attribute-key]', key)
+                .setValue('input[name=attribute-value]', editor.extra[key])
+                .verify.valueContains('input[name=attribute-key]', key)
+                .verify.valueContains('input[name=attribute-value]', editor.extra[key]);
+        }
+    },
+
+    'Create text field': function (browser) {
+        var field = editor.fields[0];
+        var type = field.type;
+        browser
+            .click('.dropdownMenu')
+            .click('a[title='+field.type+']')
+            .waitForElementVisible('.fieldcontain-'+field.type, 1000)
+            .clearValue('#'+type+'-1 input[name=label]')
+            .setValue('#'+type+'-1 input[name=label]', field.label)
+            .verify.valueContains('#'+type+'-1 input[name=label]', field.label)
+            .clearValue('#'+type+'-1 input[name=prefix]')
+            .setValue('#'+type+'-1 input[name=prefix]', field.properties.prefix)
+            .verify.valueContains('#'+type+'-1 input[name=prefix]', field.properties.prefix)
+            .clearValue('#'+type+'-1 input[name=placeholder]')
+            .setValue('#'+type+'-1 input[name=placeholder]', field.properties.placeholder)
+            .verify.valueContains('#'+type+'-1 input[name=placeholder]', field.properties.placeholder)
+            .clearValue('#'+type+'-1 input[name=max-chars]')
+            .setValue('#'+type+'-1 input[name=max-chars]', field.properties["max-chars"])
+            .verify.valueContains('#'+type+'-1 input[name=max-chars]', field.properties["max-chars"]);
+
+        if(field.required){
+            browser
+                .click('#text-1 input[name=required]')
+                .assert.attributeEquals("#text-1 input[name=required]", "checked", "true");
+        }
+
+        if(field.persistent){
+            browser
+                .click('#text-1 input[name=persistent]')
+                .assert.attributeEquals("#text-1 input[name=persistent]", "checked", "true");
+        }
+    },
+
+    'Create range field': function (browser) {
+        var field = editor.fields[5];
+        var type = field.type;
+        browser
+            .click('.dropdownMenu')
+            .click('a[title='+type+']')
+            .waitForElementVisible('.fieldcontain-'+type, 1000)
+            .clearValue('#'+type+'-1 input[name=label]')
+            .setValue('#'+type+'-1 input[name=label]', field.label)
+            .verify.valueContains('#'+type+'-1 input[name=label]', field.label)
+            .clearValue('#'+type+'-1 input[name=step]')
+            .setValue('#'+type+'-1 input[name=step]', field.properties.step)
+            .verify.valueContains('#'+type+'-1 input[name=step]', field.properties.step)
+            .clearValue('#'+type+'-1 input[name=min]')
+            .setValue('#'+type+'-1 input[name=min]', field.properties.min)
+            .verify.valueContains('#'+type+'-1 input[name=min]', field.properties.min)
+            .clearValue('#'+type+'-1 input[name=max]')
+            .setValue('#'+type+'-1 input[name=max]', field.properties.max)
+            .verify.valueContains('#'+type+'-1 input[name=max]', field.properties.max);
+
+        if(field.required){
+            browser
+                .click('#'+type+'-1 input[name=required]')
+                .assert.attributeEquals("#"+type+"-1 input[name=required]", "checked", "true");
+        }
+
+        if(field.persistent){
+            browser
+                .click('#'+type+'-1 input[name=persistent]')
+                .assert.attributeEquals("#"+type+"-1 input[name=persistent]", "checked", "true");
+        }
 
     },
 
-    'Create a text field': function (browser) {
+    'Create radio field': function (browser) {
+        var field = editor.fields[7];
+        var type = field.type;
         browser
             .click('.dropdownMenu')
-            .click('a[title=text]')
-            .waitForElementVisible('.fieldcontain-text', 1000)
+            .click('a[title='+type+']')
+            .waitForElementVisible('.fieldcontain-'+type, 1000)
+            .clearValue('#'+type+'-1 input[name=label]')
+            .setValue('#'+type+'-1 input[name=label]', field.label)
+            .verify.valueContains('#'+type+'-1 input[name=label]', field.label);
+
+        field.properties.options.forEach(function(element, index){
+            var i = index + 1;
+            browser
+                .click('.add-radio')
+                .clearValue('#'+type+'-1-'+i)
+                .setValue('#'+type+'-1-'+i, element.value)
+                .verify.valueContains('#'+type+'-1-'+i, element.value);
+        });
+
+        if(field.required){
+            browser
+                .click('#'+type+'-1 input[name=required]')
+                .assert.attributeEquals("#"+type+"-1 input[name=required]", "checked", "true");
+        }
+
+        if(field.persistent){
+            browser
+                .click('#'+type+'-1 input[name=persistent]')
+                .assert.attributeEquals("#"+type+"-1 input[name=persistent]", "checked", "true");
+        }
+
     }
+
 };
