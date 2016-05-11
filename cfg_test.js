86f98e9a3855dc718236491138a426fe73d227c9
panterz <panagiwtis.terzis@gmail.com>
2016-05-11 11:00:09 +0100

    #18: remove forms-list key from cfg.js

--- cfg_test.js
+++ cfg_test.js
@@ -3,7 +3,6 @@ var cfg = {
         "pcapianonymous": "00000000-0000-0000-0000-000000000000",
         "userid": "00000000-0000-0000-0000-000000000000",
         "version": "1.3",
-        "forms-list": true,
         "options": [
             "text",
             "range",
