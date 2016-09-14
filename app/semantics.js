09ef70f2ff61926c1b558f19c59e3f7fad67a112
George Hamilton <gmh04@netscape.net>
2016-09-14 15:29:54 +0100

    add profile attribute and require checkbox: https://github.com/edina/fieldtrip-cobweb-project/issues/152

--- app/semantics.js
+++ app/semantics.js
@@ -31,11 +31,22 @@ class Semantics {
                 'label': control.label
             });
 
+            if(control.required){
+                $("#" + id + " input[name='required']").attr('checked','checked');
+            }
+
+            // create attribute for profile
+            $('#add-attribute').click();
+
             // create each option
             $.each(control.entries, function(i, option){
                 $('#' + id + ' .add-radio').click();
                 $('#add-attribute').click();
             });
+
+            // pouplate profile attribute
+            $("input[name='attribute-key']").eq(0).val(profile.profiles[0].url);
+            $("input[name='attribute-value']").eq(0).val("profile")
 
             $.each(control.entries, function(i, option){
                 let n = i + 1;
@@ -45,8 +56,8 @@ class Semantics {
                 $('#' + oId).val(option.label);
 
                 // create attribute entries
-                $("input[name='attribute-key']").eq(i).val(option.id);
-                $("input[name='attribute-value']").eq(i).val(option.label);
+                $("input[name='attribute-key']").eq(n).val(option.id);
+                $("input[name='attribute-value']").eq(n).val(option.label);
             });
         }, this))
     }
